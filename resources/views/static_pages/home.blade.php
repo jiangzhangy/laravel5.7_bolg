55c0c8246d358f463b99c6a99c642aa179c7e94c
jiangzhangy <649768017@qq.com>
2019-01-17 14:45:15 +0800

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,9 @@
                     @include('shared._status_form')
                 </section>
             </div>
+            <h4>微博列表</h4>
+            <hr>
+            @include('shared._feed')
             <aside class="col-md-4">
                 <section class="user_info">
                     @include('shared._user_info', ['user' => Auth::user()])
