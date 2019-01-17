06016ddec50620e31840d73b935c757655874685
jiangzhangy <649768017@qq.com>
2019-01-17 09:57:06 +0800

    微博数据模型

---
+++ app/Models/Status.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    //用户和微博是一对多的数据关联
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,10 +36,20 @@ class User extends Authenticatable
         });
     }
 
+    /**
+     * 获取外部头像
+     * @param string $size
+     * @return string
+     */
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
     }
 
 }
